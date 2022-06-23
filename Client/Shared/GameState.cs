b4883ed10e5043b783a41b8eb3152c2a68b73777
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-06-23 11:25:55 -0400

    Updated with indicators and improved yellow color

--- Client/Shared/GameState.cs
+++ Client/Shared/GameState.cs
@@ -9,13 +9,27 @@ public class GameState
 	}
 
 	/// <summary>
+	/// Indicate whether a player has won, the game is a tie, or game in ongoing
+	/// </summary>
+	public enum WinState
+	{
+		No_Winner = 0,
+		Player1_Wins = 1,
+		Player2_Wins = 2,
+		Tie = 3
+	}
+
+	/// <summary>
 	/// The player whose turn it is.  By default, player 1 starts first
 	/// </summary>
-	public byte PlayerTurn = 1;
+	public int PlayerTurn => TheBoard.Count(x => x != 0) % 2 + 1;
 
-	public byte CurrentTurn { get; private set; } = 0;
+	/// <summary>
+	/// Number of turns completed and pieces played so far in the game
+	/// </summary>
+	public int CurrentTurn { get { return TheBoard.Count(x => x != 0); } }
 
-	public static readonly List<byte[]> WinningPlaces = new();
+	public static readonly List<int[]> WinningPlaces = new();
 
 	public static void CalculateWinningPlaces() 
 	{
@@ -28,7 +42,7 @@ public class GameState
 			byte checkCol = rowCol1;
 			while (checkCol <= rowColEnd-3)
 			{
-				WinningPlaces.Add(new byte[] { 
+				WinningPlaces.Add(new int[] { 
 					checkCol, 
 					(byte)(checkCol + 1), 
 					(byte)(checkCol + 2), 
@@ -48,7 +62,7 @@ public class GameState
 			byte checkRow = colRow1;
 			while (checkRow <= 14+col)
 			{
-				WinningPlaces.Add(new byte[] {
+				WinningPlaces.Add(new int[] {
 					checkRow,
 					(byte)(checkRow + 7),
 					(byte)(checkRow + 14),
@@ -69,7 +83,7 @@ public class GameState
 			byte checkPos = colRow1;
 			while (checkPos <= colRowEnd)
 			{
-				WinningPlaces.Add(new byte[] {
+				WinningPlaces.Add(new int[] {
 					checkPos,
 					(byte)(checkPos - 6),
 					(byte)(checkPos - 12),
@@ -90,7 +104,7 @@ public class GameState
 			byte checkPos = colRow1;
 			while (checkPos <= colRowEnd)
 			{
-				WinningPlaces.Add(new byte[] {
+				WinningPlaces.Add(new int[] {
 					checkPos,
 					(byte)(checkPos + 8),
 					(byte)(checkPos + 16),
@@ -108,11 +122,11 @@ public class GameState
 	/// Check the state of the board for a winning scenario
 	/// </summary>
 	/// <returns>0 - no winner, 1 - player 1 wins, 2 - player 2 wins, 3 - draw</returns>
-	public byte CheckForWin()
+	public WinState CheckForWin()
 	{
 
 		// Exit immediately if less than 7 pieces are played
-		if (TheBoard.Count(x => x != 0) < 7) return 0;
+		if (TheBoard.Count(x => x != 0) < 7) return WinState.No_Winner;
 
 		foreach (var scenario in WinningPlaces)
 		{
@@ -124,13 +138,13 @@ public class GameState
 				TheBoard[scenario[1]] ==
 				TheBoard[scenario[2]] &&
 				TheBoard[scenario[2]] ==
-				TheBoard[scenario[3]]) return TheBoard[scenario[0]];
+				TheBoard[scenario[3]]) return (WinState)TheBoard[scenario[0]];
 
 		}
 
-		if (TheBoard.Count(x => x != 0) == 42) return 3;
+		if (TheBoard.Count(x => x != 0) == 42) return WinState.Tie;
 
-		return 0;
+		return WinState.No_Winner;
 
 	}
 
@@ -139,7 +153,7 @@ public class GameState
 	/// </summary>
 	/// <param name="column">0-indexed column to place the piece into</param>
 	/// <returns>The final array index where the piece resides</returns>
-	public byte PlayPiece(byte column)
+	public int PlayPiece(int column)
 	{
 
 		// Check for a current win
@@ -158,21 +172,17 @@ public class GameState
 
 		TheBoard[landingSpot] = PlayerTurn;
 
-		PlayerTurn = PlayerTurn == 1 ? (byte)2 : (byte)1;
-		CurrentTurn++;
-
 		return landingSpot;
 
 	}
 
-	public List<byte> TheBoard { get; private set; } = new List<byte>(new byte[42]);
+	public List<int> TheBoard { get; private set; } = new List<int>(new int[42]);
 
 	public void ResetBoard() {
-		TheBoard = new List<byte>(new byte[42]);
-		PlayerTurn = 1;
+		TheBoard = new List<int>(new int[42]);
 	}
 
-	public byte ConvertLandingSpotToRow(byte landingSpot)
+	public byte ConvertLandingSpotToRow(int landingSpot)
 	{
 
 		return (byte)Math.Floor(landingSpot / (decimal)7);
